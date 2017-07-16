46eb0cd9ab5b2f91019f4aaa4cf6ef56f3384af9
Boots2291 <buteramv@gmail.com>
2017-07-16 14:22:22 -0600

    Add tests to help build

--- test/computer_test.rb
+++ test/computer_test.rb
@@ -12,5 +12,25 @@ class ComputerPlayerTest < Minitest::Test
     computer = ComputerPlayer.new
     refute computer.computer_board.grid.include?(true)
   end
-  
+
+  def test_computer_can_place_small_ship
+    skip
+    computer = ComputerPlayer.new
+    computer.computer_board.place_ship("A1 A2")
+    assert_equal true, computer.computer_board.grid[0][0]
+    assert_equal true, computer.computer_board.grid[0][1]
+    assert_equal false, computer.computer_board.grid[0][2]
+  end
+
+  def test_computer_can_place_large_ship
+    skip
+    computer = ComputerPlayer.new
+    computer.computer_board.place_ship("A1 A2 A3")
+    assert_equal true, computer.computer_board.grid[0][0]
+    assert_equal true, computer.computer_board.grid[0][1]
+    assert_equal true, computer.computer_board.grid[0][2]
+    assert_equal false, computer.computer_board.grid[1][1]
+    assert_equal false, computer.computer_board.grid[3][2]
+  end
+
 end
